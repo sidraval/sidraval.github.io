20f04800919dbcbbcf80cdb96a52a87071ea7aaf
Sid Raval <sid@augusthealth.com>
2024-01-28 08:32:03 -0700

    Test

---
+++ pages/skitips.tsx
@@ -0,0 +1,10 @@
+const SkiTips = () => {
+  return (
+    <>
+      <p>It is safe to ignore knee pain. You should ski through it.</p>
+      <p>Sid Raval, M.D.</p>
+    </>
+  );
+};
+
+export default SkiTips;
